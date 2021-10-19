36eec1077ec797ab11387fd0fda11e8ec4b0c306
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-10-20 00:55:57 +0530

    Create Print pretty.cpp

---
+++ Print pretty.cpp
@@ -0,0 +1,10 @@
+	/* Enter your code here */
+    long long int D=(long)A;
+    cout<<hex<<showbase<<left<<nouppercase;
+    cout<<D<<endl;
+    cout<<fixed<<setw(15)<<setprecision(2)<<showpos<<right<<setfill('_');
+    cout<<B<<endl;
+    cout<<setprecision(9)<<left<<noshowpos<<uppercase<<scientific;
+    cout<<C<<endl;
+	}
+	return 0;
